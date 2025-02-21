2f8afe0b645b6726d1594802c82e516b210ad143
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2025-02-21 21:56:37 +0100

    Revert "chore: gh pages testing"
    This reverts commit 72db27ff1afa5a3b1cad88f4e943b2bde64f2a65.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://docs.qbox.re',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/qbox-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
