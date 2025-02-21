b387e873f91aef82b912f8c02a0a549aef0fe883
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2025-02-21 21:07:04 +0100

    feat: local search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,6 +31,22 @@ const config: Config = {
     locales: ['en'],
   },
 
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
+
   presets: [
     [
       'classic',
