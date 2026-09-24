2802f3de2126d6a074844a488c395b1f6c06edb6
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2026-09-24 18:05:51 +0200

    fix: self-host the favicon and social preview image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Qbox Documentation',
   tagline: 'The official documentation for the Qbox Project',
-  favicon: 'https://files.fivemerr.com/images/b02ac973-403b-4ead-99bf-17bc307172d9.png',
+  favicon: 'favicon.ico',
   trailingSlash: false,
   // Set the production url of your site here
   url: 'https://docs.qbox.re',
@@ -150,8 +150,7 @@ const config: Config = {
         scrollOffset: 0,
       }
     },
-    // Replace with your project's social card
-    // image: 'img/qbox-logo2.jpg',
+    image: 'img/qbox-logo2.png',
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: false,
@@ -159,7 +158,6 @@ const config: Config = {
     },
     metadata: [
       { name: 'keywords', content: 'qbox, qbus, qbus project, qbus documentation, qbox documentation, qbox project, fivem, redm, cfx, citizenfx, fivem framework' },
-      { name: 'og:image', content: 'https://files.fivemerr.com/images/b02ac973-403b-4ead-99bf-17bc307172d9.png' },
     ],
     algolia: {
       appId: '7JRFEYG1CO',
